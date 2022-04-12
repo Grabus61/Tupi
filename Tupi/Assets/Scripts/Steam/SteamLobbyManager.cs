284a0a8bedd6efc46f8ca7312093575536ec5635
Grabus61 <bugragedikli024@gmail.com>
2022-04-12 15:32:48 +0300

    Leave Lobby & Before Mirror Import

--- Tupi/Assets/Scripts/Steam/SteamLobbyManager.cs
+++ Tupi/Assets/Scripts/Steam/SteamLobbyManager.cs
@@ -39,6 +39,7 @@ public class SteamLobbyManager : MonoBehaviour
         //Callback Events
         Steamworks.SteamMatchmaking.OnLobbyCreated += OnLobbyCreated;
         Steamworks.SteamMatchmaking.OnLobbyMemberJoined += OnLobbyMemberJoined;
+        Steamworks.SteamMatchmaking.OnLobbyMemberLeave += OnLobbyMemberLeave;
         Steamworks.SteamMatchmaking.OnLobbyEntered += OnLobbyEntered;
         Steamworks.SteamMatchmaking.OnChatMessage += OnChatMessage;
         Steamworks.SteamFriends.OnGameLobbyJoinRequested += OnGameLobbyJoinRequested;    
@@ -60,26 +61,37 @@ public class SteamLobbyManager : MonoBehaviour
     public async void OnLobbyMemberJoined(Lobby lobby, Friend friend){
         Debug.Log($"{friend.Name} joined the lobby");
         GameObject obj = Instantiate(inLobbyFriend, playerContent);
+        obj.name = friend.Name;
         obj.GetComponentInChildren<TMP_Text>().text = friend.Name;
         obj.GetComponentInChildren<RawImage>().texture = await SteamFriendsManager.GetTextureFromSteamIdAsync(friend.Id);
         inLobby.Add(friend.Id, obj);
     }
 
-    public async void OnLobbyEntered(Lobby lobby){
-        Debug.Log("Client Entered the Lobby!");
-        GameObject obj = Instantiate(inLobbyFriend, playerContent);
-        obj.GetComponentInChildren<TMP_Text>().text = SteamClient.Name;
-        obj.GetComponentInChildren<RawImage>().texture = await SteamFriendsManager.GetTextureFromSteamIdAsync(SteamClient.SteamId);
-        inLobby.Add(SteamClient.SteamId, obj);
+    public void OnLobbyMemberLeave(Lobby lobby, Friend friend){
+        Destroy(inLobby[friend.Id].gameObject);
+        inLobby.Remove(friend.Id);
 
+        GameObject messageObj = Instantiate(chatMessage, chatContent);
+        messageObj.GetComponentInChildren<TMP_Text>().text = $"<b>{friend.Name}</b> left the lobby!";
+    }
+
+    public async void OnLobbyEntered(Lobby lobby){
         foreach(var friend in currentLobby.Members){
             if(friend.Id != SteamClient.SteamId){
                 GameObject friendobj = Instantiate(inLobbyFriend, playerContent);
+                friendobj.name = friend.Name;
                 friendobj.GetComponentInChildren<TMP_Text>().text = friend.Name;
                 friendobj.GetComponentInChildren<RawImage>().texture = await SteamFriendsManager.GetTextureFromSteamIdAsync(friend.Id);
                 inLobby.Add(friend.Id, friendobj);
             }
-        }       
+        }   
+
+        Debug.Log("Client Entered the Lobby!");
+        GameObject obj = Instantiate(inLobbyFriend, playerContent);
+        obj.name = SteamClient.Name;
+        obj.GetComponentInChildren<TMP_Text>().text = SteamClient.Name;
+        obj.GetComponentInChildren<RawImage>().texture = await SteamFriendsManager.GetTextureFromSteamIdAsync(SteamClient.SteamId);
+        inLobby.Add(SteamClient.SteamId, obj);      
     }
 
     public async void OnGameLobbyJoinRequested(Lobby joinedLobby, SteamId id){
@@ -122,6 +134,12 @@ public class SteamLobbyManager : MonoBehaviour
             currentLobby.SetJoinable(true);
             currentLobby.SetData("name", name);
             currentLobby.SetData("game", "tupi");
+
+            foreach(var user in inLobby.Values){
+                Destroy(user);
+            }
+            inLobby.Clear();
+
             return true;
         }
         catch (System.Exception e )
@@ -146,9 +164,14 @@ public class SteamLobbyManager : MonoBehaviour
             chatInput.ActivateInputField();
         }
     }
+    
 
     public async void SearchForLobby(){
         Lobby[] foundLobbies = await SteamMatchmaking.LobbyList.WithKeyValue("game", "tupi").RequestAsync();
+
+        foreach(Transform obj in lobbyContent){
+            Destroy(obj.gameObject);
+        }
 
         foreach(Lobby lobby in foundLobbies){
             var obj = Instantiate(lobbyObject, lobbyContent);
@@ -168,6 +191,22 @@ public class SteamLobbyManager : MonoBehaviour
         else{
             currentLobby = lobby;
             OnJoin.Invoke();
+        }
+    }
+
+    public void LeaveLobby(){
+        try
+        {
+            currentLobby.Leave();
+            OnLeave.Invoke();
+            foreach(var user in inLobby.Values){
+                Destroy(user);
+            }
+            inLobby.Clear();
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogError("Lobby Leave Failed: " + e.Message);
         }
     }
 
